c86696e0676b9b7325d18f8a9ae8a0b71a662e04
Walid Nasim <wanasim92@gmail.com>
2025-01-19 18:21:04 -0500

    emit EnterRaffle event

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -9,12 +9,24 @@ pragma solidity ^0.8.19;
  */
 contract Raffle {
     uint256 private immutable i_entranceFee;
+    address payable[] private s_players;
+
+    error Raffle_NotEnoughEthSent();
+
+    event EnteredRaffle(address indexed player);
 
     constructor(uint256 entranceFee) {
         i_entranceFee = entranceFee;
     }
 
-    function enterRaflle() public payable {}
+    function enterRaflle() external payable {
+        // require(msg.value >= i_entranceFee, "Not enough ETH");
+        if (msg.value < i_entranceFee) {
+            revert Raffle_NotEnoughEthSent();
+        }
+        s_players.push(payable(msg.sender));
+        emit EnteredRaffle(msg.sender);
+    }
 
     function pickWinner() public {}
 
