ba5002e1ce3ccaf71148e0ca6db07be50ae54c3a
Walid Nasim <wanasim92@gmail.com>
2025-01-21 20:31:00 -0500

    adding initial VRF logic

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -1,22 +1,62 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+import {VRFConsumerBaseV2Plus} from "chainlink-brownie-contracts/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+import {VRFV2PlusClient} from "chainlink-brownie-contracts/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
+
 /**
  * @title Raffle Contract
  * @author Walid Nasim
  * @notice Contract for creating raffle
  * @dev Implements Chainlink VRF v2.5
  */
-contract Raffle {
+contract Raffle is VRFConsumerBaseV2Plus {
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
+
+    /**
+     * Type Declarations
+     */
+    enum RaffleState {
+        OPEN,
+        CALCULATING
+    }
+
+    /**
+     * State Variables
+     */
     uint256 private immutable i_entranceFee;
+    uint256 private immutable i_interval;
+    uint256 private immutable i_lastTimeStamp;
+    bytes32 private immutable i_keyHash; // Gas Price Limit. Functions as an ID for the offchain VRF job
+    uint256 private immutable i_subscriptionId;
+    uint32 private immutable i_callbackGasLimit;
+
+    address payable private s_winner;
     address payable[] private s_players;
+    RaffleState private s_raffleState;
 
     error Raffle_NotEnoughEthSent();
+    error Raffle_TransferFailed();
+    error Raffle_StateNotOpen();
 
     event EnteredRaffle(address indexed player);
 
-    constructor(uint256 entranceFee) {
+    constructor(
+        uint256 entranceFee,
+        uint256 interval,
+        address vrfCoordinator,
+        bytes32 gasLane,
+        uint256 subscriptionId,
+        uint32 callbackGasLimit
+    ) VRFConsumerBaseV2Plus(vrfCoordinator) {
         i_entranceFee = entranceFee;
+        i_interval = interval;
+        i_lastTimeStamp = block.timestamp;
+        i_keyHash = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
+        s_raffleState = RaffleState.OPEN;
     }
 
     function enterRaflle() external payable {
@@ -24,13 +64,54 @@ contract Raffle {
         if (msg.value < i_entranceFee) {
             revert Raffle_NotEnoughEthSent();
         }
+
+        if (s_raffleState != RaffleState.OPEN) {
+            revert Raffle_StateNotOpen();
+        }
         s_players.push(payable(msg.sender));
         emit EnteredRaffle(msg.sender);
     }
 
-    function pickWinner() public {}
+    function pickWinner() external {
+        if (block.timestamp - i_lastTimeStamp >= i_interval) {
+            revert();
+        }
+
+        s_raffleState = RaffleState.CALCULATING;
+
+        /**
+         * Get Random Number
+         * 1. Request RNG
+         * 2. Get Random Number
+         */
+        VRFV2PlusClient.RandomWordsRequest memory request = VRFV2PlusClient.RandomWordsRequest({
+            keyHash: i_keyHash,
+            subId: i_subscriptionId,
+            requestConfirmations: REQUEST_CONFIRMATIONS,
+            callbackGasLimit: i_callbackGasLimit,
+            numWords: NUM_WORDS,
+            extraArgs: VRFV2PlusClient._argsToBytes(
+                // Set nativePayment to true to pay for VRF requests with Sepolia ETH instead of LINK
+                VRFV2PlusClient.ExtraArgsV1({nativePayment: false})
+            )
+        });
+
+        uint256 requestId = s_vrfCoordinator.requestRandomWords(request);
+    }
 
     function getEntranceFee() public view returns (uint256) {
         return i_entranceFee;
+    }
+
+    function fulfillRandomWords(uint256 requestId, uint256[] calldata randomWords) internal override {
+        // Pick Winner
+        uint256 winnerIndex = randomWords[0] % s_players.length;
+        address payable winner = s_players[winnerIndex];
+        s_winner = winner;
+        s_raffleState = RaffleState.OPEN;
+        (bool success,) = s_winner.call{value: address(this).balance}("");
+        if (!success) {
+            revert Raffle_TransferFailed();
+        }
     }
 }
